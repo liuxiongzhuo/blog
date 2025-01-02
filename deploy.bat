0bbae1b147554a88ef54db4cf634bb7596e0b04d
liuxiongzhuo <3231729413@qq.com>
2025-01-02 08:50:07 +0800

    2025/01/02 周四  8:50:07.67

--- deploy.bat
+++ deploy.bat
@@ -4,4 +4,4 @@ git add .
 git commit -m "%DATE% %TIME%"
 git push
 cd C:\Program Files\PuTTY
-plink.exe -t -pw uLwvpASu7hQYI6j1xPVi root@2a14:67c0:104::3c "cd /var/lxz/blog;git pull"
+echo y | plink.exe -pw uLwvpASu7hQYI6j1xPVi root@2a14:67c0:104::3c "cd /var/lxz/blog;git pull"
